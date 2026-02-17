84ddd0e480ff9bd708c359001de58fc51c1f304a
lDraco15 <abnercito.villeda@gmail.com>
2026-02-17 14:08:17 +0100

    Refactor: Se cambio la logica de imprimir el mapa, al acabarse el tiempo no ha sido completado, en el proximo commit sera terminado

--- entregas/villedaAbner/src/ExamenMina.java
+++ entregas/villedaAbner/src/ExamenMina.java
@@ -7,35 +7,41 @@ public class ExamenMina {
 			{ 0, 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0 }
+			{ 0, 0, 0, 0, 0, 0, 0 },
 	};
 
 	// Posiciones Random de la mina
 	public static void main(String[] args) {
+		String[] tile = {
+				" - ",
+				"="
+		};
 		Scanner sc = new Scanner(System.in);
 		int colocarMinas = 0;
 		while (colocarMinas < 5) {
-			int posicionMinaY = (int) (Math.random() * 7);
-			int posicionMinaX = (int) (Math.random() * 5);
-			if(mapa[posicionMinaX][posicionMinaY] == 1){
-			posicionMinaX++;
-			posicionMinaY++;
-			mapa[posicionMinaX][posicionMinaY] = 1;
+			int posicionMinaY = (int) (Math.random() * 6);
+			int posicionMinaX = (int) (Math.random() * 4);
+			if (mapa[posicionMinaX][posicionMinaY] == 1) {
+				posicionMinaX++;
+				posicionMinaY++;
+				mapa[posicionMinaX][posicionMinaY] = 1;
 
-			}else{
-			mapa[posicionMinaX][posicionMinaY] = 1;
+			} else {
+				mapa[posicionMinaX][posicionMinaY] = 1;
 			}
 			colocarMinas++;
 		}
 
 		int posicionX = 0, posicionY = 0, haTerminado = 1, valoresCorrectos, contadorMapa = 0, contadorMinas = 0;
-		///////////// Comienza el juego
 		while (haTerminado == 1) {
-			for (int i = 0; i < mapa.length; i++) {
-				for (int j = 0; j <= mapa.length + 1; j++) {
-					System.out.print(mapa[i][j]);
+			for (int row = 0; row < mapa.length; row++) {
+				System.out.print(" " + row + " ");
+				for (int column = 0; column < mapa[row].length; column++) {
+					if (mapa[row][column] == 0 || mapa[row][column] == 1) {
+						System.out.print(tile[0]);
+					}
 				}
-				System.out.println(" ");
+				System.out.println();
 			}
 			valoresCorrectos = 0;// reseteo de la variable
 			while (valoresCorrectos == 0) {

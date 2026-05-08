34036db2ce57025ac538f56aea11a63ddf07f8db
lDraco15 <abnercito.villeda@gmail.com>
2026-05-08 13:55:49 +0200

    feat: clase menu implementada

--- entregas/villedaAbner/src/Menu.java
+++ entregas/villedaAbner/src/Menu.java
@@ -1,13 +1,18 @@
 public class Menu {
+    private Console console;
 
     public int pediropcion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pediropcion'");
+        return console.readInt();
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        console.writeln("--------------------");
+        console.writeln("1. Pedir");
+        console.writeln("2. Empezar de nuevo");
+        console.writeln("3. Salir");
+        console.writeln("--------------------");
+
+
     }
 
 }
